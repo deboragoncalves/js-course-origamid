592133a5b1d96fdb406696f93278440067a7f55c
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-04-02 18:55:15 -0300

    Navegação por tab - Competições

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     addLinksMenu();
     addAltImg();
+    showCompetitions();
 };
 
 const addLinksMenu = () => {
@@ -18,8 +19,8 @@ const addLinksMenu = () => {
     if (textMenu) {
       textMenu = textMenu.toUpperCase();
 
-        if (textMenu.includes("SUPERLIGA")) {
-            item.setAttribute("href", "#superliga");
+        if (textMenu.includes("COMPETIÇÕES")) {
+            item.setAttribute("href", "#competitions");
         } else if (textMenu.includes("DÚVIDAS")) {
             item.setAttribute("href", "#faq");
         } else if (textMenu.includes("CONTATO")) {
@@ -45,10 +46,34 @@ const addAltImg = () => {
 
             if (srcAttribute.includes("MAPA")) {
                 image.setAttribute("alt", "Mapa com a localização da Confederação Brasileira de Vôlei")
-            } else if (srcAttribute.includes("SUPERLIGA")) {
-                image.setAttribute("alt", "Ginásio de vôlei");
             }
         }
     })
     
+}
+
+const showCompetitions = () => {
+    let listNamesCompetitions = document.querySelectorAll(".list-competitions li");
+
+    // Ao clicar na li, adicionar classe ativo
+    listNamesCompetitions.forEach((nameCompetition, index) => {
+        nameCompetition.addEventListener('click', () => {
+            addClassActive(index);
+        });
+    });
+
+    // Primeiro elemento do item, ao carregar a página exibe a sua descrição, iniciando com a classe ativo
+    let listDescriptionsCompetitions = document.querySelectorAll(".list-descriptions li");
+    listDescriptionsCompetitions[0].classList.add("active");
+};
+
+const addClassActive = index => {
+    let listDescriptionsCompetitions = document.querySelectorAll(".list-descriptions li");
+
+    // Remover as classes ativo de todos os itens
+    listDescriptionsCompetitions.forEach(description => {
+        description.classList.remove("active");
+    });
+
+    listDescriptionsCompetitions[index].classList.add("active");
 }
