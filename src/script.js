d07a0f0643fc7ec668e3ef1cdf760a2d6f6f7389
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-04-02 19:31:11 -0300

    Accordion list - Perguntas e respostas

--- src/script.js
+++ src/script.js
@@ -2,14 +2,17 @@ window.onload = () => {
     addLinksMenu();
     addAltImg();
     showCompetitions();
+    getQuestions();
 };
+
+const classActive = "active";
 
 const addLinksMenu = () => {
   // Adicionar classe em cada li e selecionar todas com querySelectorAll, retornando uma nodeList
 
   let listMenu = document.querySelectorAll(".nav-link");
 
-  listMenu.forEach((item, index) => {
+  listMenu.forEach(item => {
     if (item.hasAttribute("href")) {
       return;
     }
@@ -54,6 +57,9 @@ const addAltImg = () => {
 
 const showCompetitions = () => {
     let listNamesCompetitions = document.querySelectorAll(".list-competitions li");
+    let listDescriptionsCompetitions = document.querySelectorAll(".list-descriptions li");
+
+    if (listNamesCompetitions.length <= 0 || listDescriptionsCompetitions.length <= 0) return;
 
     // Ao clicar na li, adicionar classe ativo
     listNamesCompetitions.forEach((nameCompetition, index) => {
@@ -63,8 +69,7 @@ const showCompetitions = () => {
     });
 
     // Primeiro elemento do item, ao carregar a página exibe a sua descrição, iniciando com a classe ativo
-    let listDescriptionsCompetitions = document.querySelectorAll(".list-descriptions li");
-    listDescriptionsCompetitions[0].classList.add("active");
+    listDescriptionsCompetitions[0].classList.add(classActive);
 };
 
 const addClassActive = index => {
@@ -72,8 +77,33 @@ const addClassActive = index => {
 
     // Remover as classes ativo de todos os itens
     listDescriptionsCompetitions.forEach(description => {
-        description.classList.remove("active");
+        description.classList.remove(classActive);
     });
 
-    listDescriptionsCompetitions[index].classList.add("active");
+    listDescriptionsCompetitions[index].classList.add(classActive);
 }
+
+const getQuestions = () => {
+    // Selecionar todas as perguntas - dt 
+    let listQuestions = document.querySelectorAll(".list-questions dt");
+
+    if (listQuestions.length <= 0) return;
+
+    listQuestions.forEach(question => {
+        question.addEventListener("click", showAnswer);
+    });
+
+    // Mostrar primeira resposta no onload
+    let listAnswers = document.querySelectorAll(".list-questions dd");
+    listAnswers[0].classList.add(classActive);
+}
+
+const showAnswer = event => {
+    // Item clicado
+    let questionClicked = event.currentTarget;
+    let answer = questionClicked.nextElementSibling;
+    answer.classList.toggle(classActive);
+}
+
+// TODO: Adicionar imagens às competições
+// TODO: Responsivo - matchMedia
