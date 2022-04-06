b646766a2741c2e14cb132e658490c50e516baf1
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-04-06 20:25:44 -0300

    Exercicio Objetos

---
+++ exercises/objetos/objetos/script.js
@@ -0,0 +1,9 @@
+// Exercício 01: Listar 5 objetos nativos
+
+let answerExercise01 = document.querySelector("#exercise-01-answer");
+answerExercise01.innerHTML = "String, Object, Number, Boolean, Array";
+
+// Exercício 02: Listar 5 objetos do browser
+// Se não existir no browser, erro no console
+let answerExercise02 = document.querySelector("#exercise-02-answer");
+answerExercise01.innerHTML = "Element, HTMLCollection, NodeList, Window, Document, Console";
