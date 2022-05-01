62313eebd382df573991287904ef6e06a2657daa
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-05-01 10:23:50 -0300

    Exibir titulos das seleções usando setInterval

--- final-project/src/js/script.js
+++ final-project/src/js/script.js
@@ -6,6 +6,7 @@ import modalLogin from './modules/modal-login.js';
 import mapTooltip from './modules/map-tooltip.js';
 import dropdownMenu from './modules/dropdown-menu.js';
 import menuMobile from './modules/menu-mobile.js';
+import numberTitles from './modules/number-titles.js';
 
 window.onload = () => {
     addLinksMenu();
@@ -16,10 +17,12 @@ window.onload = () => {
     mapTooltip();
     dropdownMenu();
     menuMobile();
+    numberTitles();
 };
 
 /* TODO: Adicionar imagens às competições
 TODO: Responsivo - matchMedia
 TODO: Funcionalidade login
 TODO: Toggle dropdown menu mobile
-TODO: Fechar menu dropdown/mobile quando clicar fora */
+TODO: Fechar menu dropdown/mobile quando clicar fora
+TODO: Iniciar contagem de titulos apenas quando o usuário estiver navegando na section */
