7e59f29d7c9f73a94ba03cb1fb438e28bfe46f42
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-07-03 11:29:13 -0300

    Refatorar para classe o script que exibe as competições

--- final-project/src/js/modules/show-competitions.js
+++ final-project/src/js/modules/show-competitions.js
@@ -1,34 +1,36 @@
-const addClassActive = (index) => {
-    const classActive = 'active';
+class ShowCompetitions {
+    constructor(competitions, descriptions) {
+        // .list-competitions li
+        this.listNamesCompetitions = document.querySelectorAll(competitions);
+        // .list-descriptions li
+        this.listDescriptionsCompetitions = document.querySelectorAll(descriptions);
+    }
 
-    const listDescriptionsCompetitions = document.querySelectorAll('.list-descriptions li');
+    addClassActive = (index) => {
+        const classActive = 'active';
 
-    // Remover as classes ativo de todos os itens
-    listDescriptionsCompetitions.forEach((description) => {
-        description.classList.remove(classActive);
-    });
-
-    listDescriptionsCompetitions[index].classList.add(classActive);
-};
-
-const showCompetitions = () => {
-    const classActive = 'active';
-
-    const listNamesCompetitions = document.querySelectorAll('.list-competitions li');
-    const listDescriptionsCompetitions = document.querySelectorAll('.list-descriptions li');
-
-    if (listNamesCompetitions.length <= 0 || listDescriptionsCompetitions.length <= 0) return;
-
-    // Ao clicar na li, adicionar classe ativo
-    listNamesCompetitions.forEach((nameCompetition, index) => {
-        nameCompetition.addEventListener('click', () => {
-            addClassActive(index);
+        // Remover as classes ativo de todos os itens
+        this.listDescriptionsCompetitions.forEach((description) => {
+            description.classList.remove(classActive);
         });
-    });
 
-    /* Primeiro elemento do item, ao carregar a página exibe a sua descrição,
-    iniciando com a classe ativo */
-    listDescriptionsCompetitions[0].classList.add(classActive);
-};
+        this.listDescriptionsCompetitions[index].classList.add(classActive);
+    };
 
-export default showCompetitions;
+    initShowCompetitions = () => {
+        const classActive = 'active';
+        if (this.listNamesCompetitions.length <= 0
+            || this.listDescriptionsCompetitions.length <= 0) return;
+        // Ao clicar na li, adicionar classe ativo
+        this.listNamesCompetitions.forEach((nameCompetition, index) => {
+            nameCompetition.addEventListener('click', () => {
+                this.addClassActive(index);
+            });
+        });
+        /* Primeiro elemento do item, ao carregar a página exibe a sua descrição,
+        iniciando com a classe ativo */
+        this.listDescriptionsCompetitions[0].classList.add(classActive);
+    };
+}
+
+export default ShowCompetitions;
--- final-project/src/js/script.js
+++ final-project/src/js/script.js
@@ -1,6 +1,5 @@
 import addLinksMenu from './modules/links-navbar.js';
 import addAltImg from './modules/alt-images.js';
-import showCompetitions from './modules/show-competitions.js';
 import modalLogin from './modules/modal-login.js';
 import mapTooltip from './modules/map-tooltip.js';
 import dropdownMenu from './modules/dropdown-menu.js';
@@ -9,15 +8,17 @@ import titles from './modules/titles.js';
 import timeOperation from './modules/time-operation.js';
 import watchGames from './modules/watch-games.js';
 import countDownOlympicGames from './modules/countdown-olympic-games.js';
+import ShowCompetitions from './modules/show-competitions.js';
 import ShowAnswers from './modules/show-answers.js';
 
 window.onload = () => {
     const showAnswers = new ShowAnswers('.list-questions');
     showAnswers.initShowAnswers();
 
+    const showCompetitions = new ShowCompetitions('.list-competitions li', '.list-descriptions li');
+    showCompetitions.initShowCompetitions();
     addLinksMenu();
     addAltImg();
-    showCompetitions();
     modalLogin();
     mapTooltip();
     dropdownMenu();
